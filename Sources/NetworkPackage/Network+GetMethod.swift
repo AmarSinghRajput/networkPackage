7628bdd1a46968b0478604e5539cdff9dc51eb23
Amar Singh <amarks@dewsolutions.in>
2023-08-02 09:41:01 +0530

    changes

--- Sources/NetworkPackage/Network+GetMethod.swift
+++ Sources/NetworkPackage/Network+GetMethod.swift
@@ -1,47 +1,30 @@
 //
-//  Network.swift
+//  File.swift
+//  
 //
-//
-//  Created by Amar Kumar Singh on 31/07/23.
+//  Created by Amar Kumar Singh on 01/08/23.
 //
 
 import Foundation
 
-public class Network {
-    
-    // MARK: - Properties
-    
-    public static let shared = Network()
-    private init() {}
-    public var excludedEndpointsProvider: (() -> [any APIEndPointProtocol])?
-    
-    // MARK: - POST Method with JSON request body
-    
-    public func post<T: Decodable, E: Encodable>(endpoint: any APIEndPointProtocol, body: E, isMock: Bool = false, completion: @escaping (Result<T, APIError>) -> Void) {
+extension Network {
+    public func get<T: Decodable>(endpoint: any APIEndPointProtocol, isMock: Bool = false, completion: @escaping (Result<T, APIError>) -> Void) {
         // Create the URL for the request
         if isMock {
-            guard (endpoint.mock_endpoint != nil) else {return}
+            guard (endpoint.mock_endpoint != nil) else {
+                completion(.failure(.invalidURL))
+                return
+            }
         }
         guard let url = URL(string: "\(isMock ? endpoint.mock_endpoint! : endpoint.endpoint)") else {
             completion(.failure(.invalidURL))
             return
         }
-        
-        // Create the URLRequest object with the POST method and the JSON content type
+
+        // Create the URLRequest object with the GET method
         var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        // Encode the request body as JSON data
-        do {
-            let encoder = JSONEncoder()
-            let jsonData = try encoder.encode(body)
-            request.httpBody = jsonData
-        } catch {
-            completion(.failure(.encodingFailed(error)))
-            return
-        }
-        
+        request.httpMethod = "GET"
+
         // Create a URLSessionDataTask to make the request
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
             // Handle any errors
@@ -49,19 +32,19 @@ public class Network {
                 completion(.failure(.networkError(error)))
                 return
             }
-            
+
             // Ensure that we have a valid HTTP response
             guard let httpResponse = response as? HTTPURLResponse else {
                 completion(.failure(.invalidResponse))
                 return
             }
-            
+
             // Ensure that we have valid response data
             guard let data = data else {
                 completion(.failure(.noData))
                 return
             }
-            
+
             // Check the status code of the HTTP response
             switch httpResponse.statusCode {
             case 200..<300:
@@ -73,8 +56,8 @@ public class Network {
                 } catch {
                     if selectedEnvironment == .production || selectedEnvironment == .staging {
                         completion(.failure(.decodingFailed(error)))
-                    }else {
-                        //perform nested mock API or JSON reading on dev only
+                    } else {
+                        // Perform nested mock API or JSON reading on dev only
                         if let excludedEndpointsProvider = self.excludedEndpointsProvider {
                             let excludedEndpoints = excludedEndpointsProvider()
                             let containsExcludedEndpoint = excludedEndpoints.contains { excludedEndpoint in
@@ -82,12 +65,12 @@ public class Network {
                             }
                             if containsExcludedEndpoint {
                                 completion(.failure(.httpError(statusCode: httpResponse.statusCode)))
-                            }else{
-                                self.handleMockPostAPI(endpoint: endpoint, body: body, isMock: isMock, completion: completion)
+                            } else {
+                                self.handleMockGETAPI(endpoint: endpoint, isMock: isMock, completion: completion)
                                 return
                             }
-                        }else {
-                            self.handleMockPostAPI(endpoint: endpoint, body: body, isMock: isMock, completion: completion)
+                        } else {
+                            self.handleMockGETAPI(endpoint: endpoint, isMock: isMock, completion: completion)
                             return
                         }
                     }
@@ -96,8 +79,8 @@ public class Network {
                 // There was an error with the API on production, return the error
                 if selectedEnvironment == .production || selectedEnvironment == .staging {
                     completion(.failure(.httpError(statusCode: httpResponse.statusCode)))
-                }else {
-                    // perform nested mock API or JSON reading on dev only
+                } else {
+                    // Perform nested mock API or JSON reading on dev only
                     if let excludedEndpointsProvider = self.excludedEndpointsProvider {
                         let excludedEndpoints = excludedEndpointsProvider()
                         let containsExcludedEndpoint = excludedEndpoints.contains { excludedEndpoint in
@@ -105,12 +88,12 @@ public class Network {
                         }
                         if containsExcludedEndpoint {
                             completion(.failure(.httpError(statusCode: httpResponse.statusCode)))
-                        }else {
-                            self.handleMockPostAPI(endpoint: endpoint, body: body, isMock: isMock, completion: completion)
+                        } else {
+                            self.handleMockGETAPI(endpoint: endpoint, isMock: isMock, completion: completion)
                             return
                         }
-                    }else {
-                        self.handleMockPostAPI(endpoint: endpoint, body: body, isMock: isMock, completion: completion)
+                    } else {
+                        self.handleMockGETAPI(endpoint: endpoint, isMock: isMock, completion: completion)
                         return
                     }
                 }
@@ -122,25 +105,22 @@ public class Network {
 }
 
 extension Network {
-    //  developed to handle a situation during development when API fails to respond
-    //  if API fails call mock API
-    // if mock fails read form JSON stored locally containing API mock responses
-    
-    func handleMockPostAPI<T: Decodable, E: Encodable>(endpoint: any APIEndPointProtocol, body: E, isMock: Bool = false, completion: @escaping (Result<T, APIError>) -> Void) {
-        //if isMock == true here it means Mock API has also failed and now we have to pick from JSON file
-        if isMock{
-            //reading from JSON here
-            guard let jsonName = endpoint.mock_json else { return }
+    func handleMockGETAPI<T: Decodable>(endpoint: any APIEndPointProtocol, isMock: Bool = false, completion: @escaping (Result<T, APIError>) -> Void) {
+        if isMock {
+            // If isMock == true, it means the Mock API has also failed, and now we have to pick data from a JSON file.
+            guard let jsonName = endpoint.mock_json else {
+                completion(.failure(.invalidURL))
+                return
+            }
             if let mockModel = JSONReader.fetchMockAPIResponse(fileName: jsonName, responseType: T.self) {
                 completion(.success(mockModel))
-            }else {
-                //reading from JSON failed
+            } else {
+                // Reading from JSON failed.
                 completion(.failure(.noData))
             }
-            //if isMock == false here it means API has failed and now we have to call MOCK API
-        }else{
-            //mock API called
-            self.post(endpoint: endpoint, body: body, isMock: true, completion: completion)
+        } else {
+            // For non-mock requests, call the GET method for non-mock requests.
+            self.get(endpoint: endpoint, isMock: false, completion: completion)
         }
     }
 }
